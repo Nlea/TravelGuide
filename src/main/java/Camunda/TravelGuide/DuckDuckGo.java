a5f53e24c13b6ed5d6ca9b88faa298e0e6bfe6f7
Felix Anhalt <felix.anhalt@camunda.com>
2018-08-16 15:02:22 +0200

    randomize outcome

--- src/main/java/Camunda/TravelGuide/DuckDuckGo.java
+++ src/main/java/Camunda/TravelGuide/DuckDuckGo.java
@@ -5,13 +5,7 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.camunda.bpm.engine.impl.util.json.JSONObject;
 
 import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
-
-import Camunda.utils.JSONWorker;
-import spinjar.com.jayway.jsonpath.JsonPath;
-
-
 
 public class DuckDuckGo implements JavaDelegate {
 
@@ -20,28 +14,27 @@ public class DuckDuckGo implements JavaDelegate {
 		// TODO Auto-generated method stub
 
 		String name = (String) exe.getVariable("city");
-		
-		
-		
-		
+
 		// These code snippets use an open-source library. http://unirest.io/java
-		HttpResponse<String> response = Unirest.get("https://duckduckgo-duckduckgo-zero-click-info.p.mashape.com/?callback=process_duckduckgo&format=json&no_html=1&no_redirect=1&q="+name+"&skip_disambig=1")
-		.header("X-Mashape-Key", "sjbEkLUmcWmshKXq0pWqXIkM6BzSp1e37zxjsnXSjAjEQfWwG4")
-		.header("Accept", "application/json")
-		.asString();
-		
+		HttpResponse<String> response = Unirest.get(
+				"https://duckduckgo-duckduckgo-zero-click-info.p.mashape.com/?callback=process_duckduckgo&format=json&no_html=1&no_redirect=1&q="
+						+ name + "&skip_disambig=1")
+				.header("X-Mashape-Key", "sjbEkLUmcWmshKXq0pWqXIkM6BzSp1e37zxjsnXSjAjEQfWwG4")
+				.header("Accept", "application/json").asString();
+
 		JSONObject jo = new JSONObject(response);
 		String myNewJson = jo.get("body").toString();
-		String theNewerJson =  myNewJson.substring(19, myNewJson.length() - 2);
+		String theNewerJson = myNewJson.substring(19, myNewJson.length() - 2);
 		jo = new JSONObject(theNewerJson);
-		//jo.get("AbstractText");
-		String def = jo.get("Abstract").toString(); //jo.toString();//jo.get("Abstract").toString();
-		if(def.isEmpty()) {
+		// jo.get("AbstractText");
+		String def = jo.get("Abstract").toString(); // jo.toString();//jo.get("Abstract").toString();
+		if (def.isEmpty()) {
 			def = "You are UNDEFINED";
 		}
-		
-		exe.setVariable("HappyDuckAppearsHappy", def);			
-			
+
+		System.out.println(def);
+		exe.setVariable("HappyDuckAppearsHappy", def);
+
 	}
 
 }
--- src/main/java/Camunda/TravelGuide/GarbageCollector.java
+++ src/main/java/Camunda/TravelGuide/GarbageCollector.java
@@ -1,28 +1,34 @@
 package Camunda.TravelGuide;
 
+import java.util.LinkedList;
+
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.json.JSONObject;
 
 import Camunda.utils.JSONWorker;
 
-
-
-public class GarbageCollector implements JavaDelegate{
+public class GarbageCollector implements JavaDelegate {
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
 		// TODO Auto-generated method stub
+		System.out.println("Collector begins to fill with itself");
+		LinkedList<String> varVals = new LinkedList<String>();
+		varVals.add(JSONWorker.getValFromExec("HappyDuckAppearsHappy", execution));
+		varVals.add(JSONWorker.getValFromExec("UrbanDef", execution));
+
+		LinkedList<Integer> usedInts = new LinkedList<Integer>();
 		JSONObject names = new JSONObject();
-		/*location
-		names = JSONWorker.addToJO(names, JSONWorker.getValFromExec("location_latitude", execution));
-		names = JSONWorker.addToJO(names, JSONWorker.getValFromExec("location_longitude", execution));
-		
-		names = JSONWorker.addToJO(names, JSONWorker.getValFromExec("HappyDuckAppearsHappy", execution));
-		*/
-		names = JSONWorker.addToJO(names, JSONWorker.getValFromExec("UrbanDef", execution));
-		
+
+		while (usedInts.size() != varVals.size()) {
+			int i = (int) (Math.random() * varVals.size());
+			if (!usedInts.contains(i)) {
+				names = JSONWorker.addToJO(names, varVals.get(i));
+				usedInts.add(i);
+				System.out.println(i + " = " + varVals.get(i));
+			}
+		}
 		execution.setVariable("vals", names.toString());
 	}
-	
 }
